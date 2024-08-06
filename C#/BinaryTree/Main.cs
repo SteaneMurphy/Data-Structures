61fb1727a020cc46035921729c5881f89ddabd8e
Steane <james@steanemurphy.com>
2024-08-06 20:21:02 +1000

    BinaryTree added, folder name changes

---
+++ C#/BinaryTree/Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+/*******************************************************************************
+ *                              BINARY TREES                                   *
+ *                               SOLUTION BY:                                  *
+ *                                 STEANE                                      *
+ *******************************************************************************/
+
+namespace DataStructures
+{
+    internal class Entry
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree newTree = new BinaryTree();
+            Random rnd = new Random();
+            Stopwatch timer = new Stopwatch();
+            int amount = 150000;
+
+            for (int i = 0; i < amount; i++)
+            {
+                if (i == amount - 1)
+                {
+                    newTree.AddNode(i + 1);
+                }
+                else 
+                {
+                    newTree.AddNode(rnd.Next());
+                }
+            }
+
+            newTree.TraverseTree(BinaryTree.TraversalType.PreOrder, BinaryTree.Method.Display);
+            newTree.VerifyTree(newTree.GetRoot());
+            newTree.TraverseTree(BinaryTree.TraversalType.PreOrder, BinaryTree.Method.Search, 150000);
+        }
+    }
+}
---
+++ C#/BinaryTree/Node.cs
@@ -0,0 +1,73 @@
+﻿namespace DataStructures
+{
+    //This is the data structure for a Node object within a linked-list. This Node stores
+    //references to its preceding and following Nodes
+    public class Node
+    {
+        #region Class Variables
+        private int data { get; set; }
+        private Node parent { get; set; }
+        private Node nextLeft { get; set; }
+        private Node nextRight { get; set; }
+        #endregion
+
+        #region Class Constructors
+        //default Node constructor, no data passed
+        public Node()
+        {
+            nextLeft = null;
+            nextRight = null;
+        }
+        //Node constructor when data passed
+        public Node(int _data)
+        {
+            nextLeft = null;
+            nextRight = null;
+            data = _data;
+        }
+        #endregion
+
+        #region Get/Set Functions
+        //as class variables are private, must be retrieved or set via the get/set functions below
+        public int GetData()
+        {
+            return data;
+        }
+
+        public void SetData(int _data)
+        {
+            data = _data;
+        }
+
+        public Node GetParent() 
+        {
+            return parent;
+        }
+
+        public void SetParent(Node _parent) 
+        {
+            parent = _parent;
+        }
+
+        public Node GetLeft()
+        {
+            return nextLeft;
+        }
+
+        public void SetLeft(Node _nextLeft)
+        {
+            nextLeft = _nextLeft;
+        }
+
+        public Node GetRight()
+        {
+            return nextRight;
+        }
+
+        public void SetRight(Node _nextRight)
+        {
+            nextRight = _nextRight;
+        }
+        #endregion
+    }
+}
